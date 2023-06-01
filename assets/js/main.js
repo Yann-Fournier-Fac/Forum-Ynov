4cf67c224ace1002a87d66a31f1f1f76e41c9127
JoshLeFou <joshua.madieu-komla@ecole2600.com>
2023-06-01 11:13:00 +0200

    Josh le 01/06/2023

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,26 @@
 const wrapper = document.querySelector('.wrapper');
-        const loginLink = document.querySelector('.login-link');
-        const registerLink = document.querySelector('.register-link');
-        const buttonLoginPopup = document.querySelector('.button__login-popup');
-        const buttonRegisterPopup = document.querySelector('.button__register-popup');
-        const closeButton = document.querySelector('.ri-close-line');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const buttonLoginPopup = document.querySelector('.button__login-popup');
+const buttonRegisterPopup = document.querySelector('.button__register-popup');
+const closeButton = document.querySelector('.ri-close-line');
 
-        registerLink.addEventListener('click', () => {
-            wrapper.classList.add('active');
-        });
+registerLink.addEventListener('click', () => {
+    wrapper.classList.add('active');
+});
 
-        loginLink.addEventListener('click', () => {
-            wrapper.classList.remove('active');
-        });
+loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
+});
 
-        buttonLoginPopup.addEventListener('click', () => {
-            wrapper.classList.add('login-popup');
-        });
+buttonLoginPopup.addEventListener('click', () => {
+    wrapper.classList.add('login-popup');
+});
 
-        closeButton.addEventListener('click', () => {
-            wrapper.classList.remove('login-popup');
-            wrapper.classList.remove('active');
-        });
+closeButton.addEventListener('click', () => {
+    wrapper.classList.remove('login-popup');
+    wrapper.classList.remove('active');
+});
                     //////// notif ////////
         const notif = document.querySelector('.notifConnected');
 
